62b34d54bb841e87e01bc29d1e3640959c1d60f5
mahdi <mohamed.mahdi@oryxnet.io>
2022-06-08 20:59:42 +0100

    change

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ app.set('view engine', 'html');
 
 
 app.get('/:filter', async (req, res) => {
+    res.setHeader("Content-Type", "text/html");
     youtube.search(req.params.filter, { type: 'channel' }).then((results) => {
         let r = results.channels.filter(vid => vid.subscribers.length === 7 && !isNaN(vid.subscribers[2]) && !isNaN(vid.subscribers[1]) && !isNaN(vid.subscribers[0]));
-            res.write(r.name+ 'dfgtgtd"');
+        r.forEach(e => {
+            res.write(`<p>` + e.name + ' | ' +  e.link + ' | '  +  e.subscribers +`</p>`, 'utf8', () => {
+            });        })
+res.end()
     });
-    res.end()
 });
 
 
